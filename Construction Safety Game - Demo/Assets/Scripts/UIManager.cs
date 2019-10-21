caa9b557809df54ce0603d0670dc17684c69e2d8
HarryPyc <yucongpan@qq.com>
2019-10-20 22:25:14 -0400

    UI & Texture

---
+++ Construction Safety Game - Demo/Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Button BtnTutorial;
+    public Button BtnTool;
+
+    public GameObject ScrollView;
+    public GameObject ItemList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        BtnTool.onClick.AddListener(delegate () {
+            ShowUI(ItemList);
+        });
+        BtnTutorial.onClick.AddListener(delegate ()
+        {
+            ShowUI(ScrollView);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowUI(GameObject UI)
+    {
+        UI.SetActive(!UI.activeSelf);
+    }
+}
